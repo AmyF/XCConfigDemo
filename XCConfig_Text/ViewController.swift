b0dbb8fb29f647c82ff930681ee5e475c5c672ad
unko <840382477@qq.com>
2019-07-27 10:39:42 +0800

    add project

---
+++ XCConfig_Text/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  XCConfig_Text
+//
+//  Created by Feida Huang on 2019/7/26.
+//  Copyright © 2019 unko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        label.text = try! Configuration.value(for: "NICKNAME")
+    }
+
+
+}
+
+enum Configuration {
+    enum Error: Swift.Error {
+        case missingKey, invalidValue
+    }
+    
+    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey:key) else {
+            throw Error.missingKey
+        }
+        
+        switch object {
+        case let value as T:
+            return value
+        case let string as String:
+            guard let value = T(string) else { fallthrough }
+            return value
+        default:
+            throw Error.invalidValue
+        }
+    }
+}
